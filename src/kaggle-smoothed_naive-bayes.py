c1cc69c524b848d0391d1047bf4d105484abc9fe
Ryan Mokarian <mokarian.m@gmail.com>
2019-10-16 18:21:49 -0400

    added kaggle-smoothed_naive-bayes.py

--- src/kaggle-smoothed_naive-bayes.py
+++ src/kaggle-smoothed_naive-bayes.py
@@ -18,6 +18,7 @@ classes = ['hockey', 'nba', 'leagueoflegends', 'soccer', 'funny', 'movies', 'ani
            'Overwatch', 'trees', 'GlobalOffensive',
            'nfl', 'AskReddit', 'gameofthrones',
            'conspiracy', 'worldnews', 'wow', 'europe', 'canada', 'Music', 'baseball']
+set_of_all_terms_in_all_classes = set()
 
 
 def test():
@@ -61,11 +62,13 @@ def compute_max_likelihood_for_all_classes(probability_of_t_given_c_all_classes,
 
 
 def compute_likelihood_for_this_class(p_of_term_belonging_to_class, prior, tokens):
+# S
+# Here log formula can be used
+# E
     """
-    p(c|d) =  (p(x1,x2,...,xn|c) * p(c)) / p(d)
-    WHERE p(x1,x2,...,xn|c) = p(x1|c)* p(x2|c)* ...p(xn|c)
+    log p(c|d) =  log(p(x1,x2,...,xn|c) + log p(c)   (note the denominator of p(d) is disregarded)
+    WHERE log p(x1,x2,...,xn|c) = log p(x1|c) + log p(x2|c)+ ... + log p(xn|c)
     prior: p(c)
-    total_d_in_class=p(d)
     :return:
     """
     p_xi_given_c = []
@@ -75,9 +78,14 @@ def compute_likelihood_for_this_class(p_of_term_belonging_to_class, prior, token
         else:
             p_xi_given_c.append(
                 0)  # never seen feature and class in training data https://en.wikipedia.org/wiki/Naive_Bayes_classifier
-    p_X_given_c = np.prod(p_xi_given_c)
-    p_X_given_c_mult_prior = np.multiply(p_X_given_c, prior)
-    return p_X_given_c_mult_prior
+    # p_X_given_c = np.prod(p_xi_given_c)
+    # p_X_given_c_mult_prior = np.multiply(p_X_given_c, prior)
+    sum_p_X_given_c = 0
+    for p in p_xi_given_c:
+        sum_p_X_given_c+= np.log(p)
+    log_p_X_given_c_plus_log_prior = sum_p_X_given_c + np.log(prior)
+
+    return log_p_X_given_c_plus_log_prior
 
 
 def compute_priors():
@@ -91,7 +99,6 @@ def compute_priors():
     data = {'post': list(df)[0], 'class': list(df)[1]}
     df_train = pd.DataFrame(data)
     priors = compute_prior_for_each_class(df_train)
-
     return priors
 
 
@@ -110,51 +117,69 @@ def train():
 def compute_probability_of_terms_for_all_classes(df_train):
     """
     This function computes probability of terms for all classes
-     p(t|c) = (# of documents having that term) / (total number of documents in this class)
+#S
+    p(t|c) = (# of frequency of the term in class c) / (total number of words in this class)
+#E
     {"hockey":  {"yoga":"0.009", "football":"0.0082" ....},
     "nba": .....
     }
     """
-    probability_of_t_given_c_all_classes = {}
+
+# below only a set of words for all classes is computed.										   
     for a_class in classes:
-        list_of_docs, set_of_words = get_bag_of_words_for_a_class(a_class, df_train)
-        probability_of_t_given_c = compute_probability_of_term_occurrence_in_this_class(list_of_docs, set_of_words)
+        list_of_all_words_in_this_class, set_of_words_for_a_class = get_bag_of_words_for_a_class(a_class, df_train)
+        set_of_all_terms_in_all_classes.update(set(set_of_words_for_a_class))
+        print("class:" + str(a_class) + " is Done for getting global terms!")
+
+    probability_of_t_given_c_all_classes = {}											 
+    for a_class in classes:
+# below, list_of_words added
+        list_of_words, set_of_words, = get_bag_of_words_for_a_class(a_class, df_train)
+# below set_of_words changed to list_of_words
+        probability_of_t_given_c = compute_probability_of_term_occurrence_in_this_class(list_of_words, list_of_words)
         probability_of_t_given_c_all_classes[a_class] = probability_of_t_given_c
         print("class:" + str(a_class) + " is Done!")
     return probability_of_t_given_c_all_classes
 
 
-def compute_probability_of_term_occurrence_in_this_class(list_of_docs, set_of_words):
+def compute_probability_of_term_occurrence_in_this_class(list_of_words_in_a_class, set_of_words_in_a_class):
     """
     This function computes the p(t|c) for that specif class
-    p(t|c) = (# of documents having that term) / (total number of documents in this class)
+#S
+    p(t|c) = (# of frequency of the term in class c) / (total number of words in this class)
+#E
     :param list_of_docs:
     :param set_of_words:
     :return: return type is similar to
     {"yoga":"0.009", "football":"0.0082" ....}
     """
     probability_of_t_given_c = {}
-    total_number_of_docs_in_c = len(list_of_docs)
-    for term in set_of_words:
+    total_number_of_words_in_c = len(list_of_words_in_a_class)
+    for term in set_of_words_in_a_class:
         term_frequency_in_documents = 0
-        for doc in list_of_docs:
+        for doc in list_of_words_in_a_class:
             if term in doc:
                 term_frequency_in_documents += 1
-        probability_of_t_given_c[term] = term_frequency_in_documents / total_number_of_docs_in_c
-
+        # probability_of_t_given_c[term] = term_frequency_in_documents / total_number_of_docs_in_c
+        probability_of_t_given_c[term] = (term_frequency_in_documents + 1)/ (total_number_of_words_in_c + len(set_of_all_terms_in_all_classes))
     return probability_of_t_given_c
 
 
 def get_bag_of_words_for_a_class(a_class, df_train):
+
     list_of_tokenized_documents_in_this_class = list()
     set_of_all_terms_in_this_class = set()
+# below list added
+    list_of_all_terms_in_this_class = list()
     for index, row in df_train.iterrows():
         if row["class"] == a_class:
             tokenized = nltk.word_tokenize(row["post"])
             pre_processed = apply_preprocessing_on_list_of_text(tokenized)
             set_of_all_terms_in_this_class.update(set(pre_processed))
-            list_of_tokenized_documents_in_this_class.append(set(pre_processed))
-    return list_of_tokenized_documents_in_this_class, set_of_all_terms_in_this_class
+# below line added
+            list_of_all_terms_in_this_class.extend(pre_processed)
+
+    return list_of_all_terms_in_this_class, set_of_all_terms_in_this_class
 
 
 def apply_preprocessing_on_list_of_text(a_list):
@@ -183,5 +208,5 @@ if __name__ == "__main__":
     First run train() method
     Then Run test() method
     """
-    #train()
-    test()
+    train()
+    # test()
