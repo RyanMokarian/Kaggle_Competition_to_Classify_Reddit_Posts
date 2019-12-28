a5f4a7121adabf413fb8a99e65e98265a4a69c25
Ryan Mokarian <mokarian.m@gmail.com>
2019-12-28 15:58:20 -0500

    report added

--- src/prebuiltclassifier.py
+++ src/prebuiltclassifier.py
@@ -8,8 +8,14 @@ from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
 from src.utils import Utils
-
+from sklearn.metrics import confusion_matrix
 from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+import pickle
+import pandas as pd
+from mlxtend.plotting import plot_confusion_matrix
 
 nltk.download('stopwords')
 english_stemmer = nltk.stem.SnowballStemmer('english')
@@ -62,6 +68,7 @@ class PreBuiltClassifier:
         preds = nb.predict(X_test)
         UTILS.generate_submission_csv(preds)
         print("Accuracy against test data:" + str(accuracy_score(y_test, preds)))
+        self.heatmap(preds, y_test)
 
     def linear_svc(self, df):
         # 0.4758008658008658
@@ -98,6 +105,8 @@ class PreBuiltClassifier:
         X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(df['post'], df['class'],
                                                                                          df.index,
                                                                                          test_size=0.33, random_state=0)
+
+
         nb = Pipeline([('vect', StemmedCountVectorizer(stop_words='english')),
                        ('tfidf', TfidfTransformer(sublinear_tf=True, smooth_idf=False)),
                        ('logistic-regression', LogisticRegression(penalty="l2")),
@@ -105,21 +114,44 @@ class PreBuiltClassifier:
         nb.fit(X_train, y_train)
         scores = cross_val_score(nb, X_train, y_train, cv=5)
         print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-        preds = nb.predict(X_test)
         # UTILS.generate_submission_csv(preds)
+        preds = nb.predict(X_test)
         print("Accuracy against test data:" + str(accuracy_score(y_test, preds)))
+
+        # draw a heat map
+        self.heatmap(preds, y_test)
+
+    def heatmap(self, preds, y_test):
+        preds_labels = np.unique(preds.tolist())
+        print('preds_labels: ', preds_labels)
+        conf_mat = confusion_matrix(y_test, preds, labels=np.unique(preds_labels))
+        annot_kws = {'fontsize': 10,
+                     'fontstyle': 'italic',
+                     'color': "k",
+                     'alpha': 0.6,
+                     'rotation': "vertical",
+                     'verticalalignment': 'center'}
+        sns.heatmap(conf_mat, annot=True, annot_kws=annot_kws, cmap="YlGnBu", fmt="d", linewidths=.5)
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.title('Confusion Matrix')
+        plt.show()
 
     def mlp_predict_test_to_file(self, df_train, df_test):
         nb = Pipeline([('vect', CountVectorizer(stop_words=self.stopwords)),
                        ('tfidf', TfidfTransformer(sublinear_tf=True, smooth_idf=False)),
                        ('clf', MLPClassifier(verbose=True, early_stopping=True)),
                        ])
-        nb.fit(df_train['post'], df_train['class'])
-
-        y_pred = nb.predict(df_test['post'])
-        UTILS.generate_submission_csv(y_pred)
-
+        X_train, X_test, y_train, y_test = train_test_split(df_train['post'], df_train['class'], random_state=53)
+        nb.fit(X_train, y_train)
+        y_pred = nb.predict(X_test)
+        # df = pd.read_pickle("data_test.pkl")
+        # data = {'post': list(df)[0], 'class': list(df)[1]}
+        # nb.fit(df_train['post'], df_train['class'])
+        # y_pred = nb.predict(df_test['post'])
+        # UTILS.generate_submission_csv(y_pred)
+        # self.heatmap(y_pred, data['class'])
+        self.heatmap(y_pred, y_test)
 
 if __name__ == "__main__":
     UTILS = Utils()
@@ -128,4 +160,6 @@ if __name__ == "__main__":
     df_train = UTILS.get_train_data()
     df_test = UTILS.get_test_data()
 
+    # PRE_BUILD_CLASSIFIER.naive_bayes_sickit(df_train)
+    # PRE_BUILD_CLASSIFIER.logistic_regression(df_train)
     PRE_BUILD_CLASSIFIER.mlp_predict_test_to_file(df_train, df_test)
